42d58c54a142ae1ff5b9ed784b9c4571b9af5eb2
ossamum <milton.ossamu@gmail.com>
2023-02-05 09:52:20 -0300

    search all tweets from user

--- utils.py
+++ utils.py
@@ -1,3 +1,8 @@
+import math
+from datetime import datetime, timedelta
+from time import sleep
+
+import pandas as pd
 import tweepy
 
 from config import (
@@ -33,3 +38,62 @@ def get_tweepy_api() -> tweepy.API:
 
     print("TWEEPY api is successfully created...")
     return tweepy_api
+
+
+def get_all_user_tweets(tweepy_client, query, years=1, days_range=10) -> pd.DataFrame:
+    """Get all tweets from a given user
+    Example of query: from: mehdizana_ -is:retweet
+    """
+
+    # select time range
+    end_time = datetime.today()
+    start_time = end_time - timedelta(days=days_range)
+
+    num_iter = math.ceil(years * 365 / days_range)
+
+    all_tweets = []
+    for i in range(num_iter):
+
+        tweets = tweepy_client.search_all_tweets(
+            query=query,
+            tweet_fields=[
+                "attachments",
+                "author_id",
+                "context_annotations",
+                "conversation_id",
+                "created_at",
+                "edit_controls",
+                "entities",
+                "geo",
+                "id",
+                "in_reply_to_user_id",
+                "lang",
+                "public_metrics",
+                "possibly_sensitive",
+                "referenced_tweets",
+                "reply_settings",
+                "source",
+                "text",
+                "withheld",
+            ],
+            start_time=start_time,
+            end_time=end_time,
+            max_results=100,
+        )
+
+        # include only valid tweets
+        if tweets.meta["result_count"] > 0:
+            all_tweets += tweets.data
+
+            # alert when there is more than 100 tweets in the considered time range
+            if tweets.meta["result_count"] > 100:
+                print(f"There are more tweets between {start_time} and {end_time}")
+
+        end_time -= timedelta(days=days_range)
+        start_time -= timedelta(days=days_range)
+
+        sleep(1)
+
+    all_tweets = pd.DataFrame(all_tweets)
+
+    return all_tweets

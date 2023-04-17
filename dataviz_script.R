c4dc79299ea466dfd49256806f027e9005330fbe
ossamum <milton.ossamu@gmail.com>
2023-04-16 21:08:01 -0300

    dataviz for bot behaviours

--- dataviz_script.R
+++ dataviz_script.R
@@ -1,10 +1,10 @@
 library(ggplot2)
 library(dplyr)
+library(corrplot)
+library(RColorBrewer)
 
-
-
+# DATA COLLECITON
 df <- read.csv('dataviz/tweets_yearmonth.csv')
-
 
 df %>% 
   ggplot(aes(x = created_at_year_month, y=tweets, fill=tweet_type)) +
@@ -12,4 +12,64 @@ df %>%
   labs(title='Number of tweets', x='Year and Month', y='Number of tweets') +
   scale_fill_discrete(name = "Tweet type") +
   theme_bw()
+
+# METHODS
+# BOT BEHAVIOUR
+df <- read.csv('dataviz/bot_dataset.csv')
+
+like <- df %>% 
+  filter(tweet_type == 'standard') %>% 
+  select(-c(tweet_type, retweet_count)) 
+
+quartiles <- quantile(like$like_count, probs=c(.25, .75), na.rm = FALSE)
+iqr <- quartiles[2] - quartiles[1]
+
+
+m <- like %>% 
+  filter(like_count < quartiles[2] + (1.5 * iqr)) %>% 
+  cor()
+
+
+corrplot(m, type="upper", order="hclust",
+         col=brewer.pal(n=8, name="RdYlBu"))
+
+
+
+retweet <- df %>% 
+  select(-c(tweet_type, like_count)) 
+
+quartiles <- quantile(retweet$retweet_count, probs=c(.25, .75), na.rm = FALSE)
+iqr <- quartiles[2] - quartiles[1]
+
+
+m <- retweet %>% 
+  filter(retweet_count < quartiles[2] + (1.5 * iqr)) %>% 
+  cor()
+
+
+corrplot(m, type="upper", order="hclust",
+         col=brewer.pal(n=8, name="RdYlBu"))
+
+
+
+
+
+
+
+bot_like
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
